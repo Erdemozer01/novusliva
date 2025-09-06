44847db0b640be15a17e7c3d813403d14ab7ef2e
Erdemozer01 <ozer246@gmail.com>
2025-09-06 05:52:02 +0300

    güncellendi

--- company/settings.py
+++ company/settings.py
@@ -91,6 +91,7 @@ DATABASES = {
         'PORT': '3306',
         # SSL SORUNUNU VE STRICT MODE UYARISINI ÇÖZEN NİHAİ BLOK
         'OPTIONS': {
+            'ssl': {'ssl-ca': '/etc/ssl/certs/ca-certificates.crt'},
             # Strict Mode uyarısını çözmek için bu satırı ekliyoruz
             'sql_mode': 'STRICT_TRANS_TABLES',
         },
